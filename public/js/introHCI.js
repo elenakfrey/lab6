31d822b27cd75ffd51d7ee71b88fe35db88ad3d7
Elena <elenakfrey@gmail.com>
2014-02-14 09:30:22 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,17 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	console.log("/project/" + idNumber);
+	$.get("/project/" + idNumber, viewProject);
+}
+
+function viewProject(result) {
+	console.log(result);
+	var projectHTML = '<img src="' + result['image'] + '" class="detailsImage">' +
+	    '<h5>' + result['date'] +
+	    '</h5><p>' + result['summary'] + '</p>'; 
+
+	$("div#project" + result['id'] + " div.details").html(projectHTML);
 }
 
 /*
@@ -35,4 +46,14 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", addColor);
+}
+
+function addColor(result) {
+	var colors = result['colors']['hex'];
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
